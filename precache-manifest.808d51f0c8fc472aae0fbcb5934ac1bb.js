7b301a783ca3fd1211d9aa93fbb78a59844f2661
Stefan Bozic <stefanbozic@gmail.com>
2020-04-12 17:43:22 +0200

    Updates

---
+++ precache-manifest.808d51f0c8fc472aae0fbcb5934ac1bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de594fd8158b5153ec38c729b1f515a9",
+    "url": "/react-githubfinder/index.html"
+  },
+  {
+    "revision": "5bff5599a318dbf26e8a",
+    "url": "/react-githubfinder/static/css/main.69d1c331.chunk.css"
+  },
+  {
+    "revision": "d4a3fcc0c52684d34faa",
+    "url": "/react-githubfinder/static/js/2.af11f70a.chunk.js"
+  },
+  {
+    "revision": "5bff5599a318dbf26e8a",
+    "url": "/react-githubfinder/static/js/main.9f787304.chunk.js"
+  },
+  {
+    "revision": "482772fffce397bc35fd",
+    "url": "/react-githubfinder/static/js/runtime-main.35ed4131.js"
+  },
+  {
+    "revision": "0c4a6d891dfdbbca47f51f349bd8ee31",
+    "url": "/react-githubfinder/static/media/404.0c4a6d89.jpg"
+  },
+  {
+    "revision": "563a7a14bc1bf7caed39d1ae319719d5",
+    "url": "/react-githubfinder/static/media/zeromatches.563a7a14.jpg"
+  }
+]);
